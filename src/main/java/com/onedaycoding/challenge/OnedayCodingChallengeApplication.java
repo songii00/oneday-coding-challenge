dee9a9f93db4a6a4c8c09e959c33fc1db9ab3c60
zoe.s <zoe.s@kakaocorp.com>
2021-10-19 19:31:42 +0900

    init

---
+++ src/main/java/com/onedaycoding/challenge/OnedayCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.onedaycoding.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnedayCodingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnedayCodingChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/onedaycoding/challenge/OnedayCodingChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onedaycoding.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnedayCodingChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
